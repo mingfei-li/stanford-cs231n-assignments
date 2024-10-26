fixed the bug of numerical / analytical gradient mismatch
bug: x's value was modified in the softmax_loss function

assignment1/cs231n/layers.py
@@ -816,44 +816,7 @@ def softmax_loss(x, y):
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     n = x.shape[0]
-    # x -= np.max(x, axis=1, keepdims=True)
-    sum_exp_x = np.sum(np.exp(x), axis=1, keepdims=True)
-    loss = np.sum(-x[np.arange(n), y] + np.log(sum_exp_x).reshape(-1))
-    loss /= n
-
-    dx = np.exp(x) / sum_exp_x
-    dx[np.arange(n), y] -= 1
-    dx /= n
-
-    # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
-    return loss, dx
-
-def softmax_loss2(x, y):
-    """
-    Computes the loss and gradient for softmax classification.
-
-    Inputs:
-    - x: Input data, of shape (N, C) where x[i, j] is the score for the jth
-      class for the ith input.
-    - y: Vector of labels, of shape (N,) where y[i] is the label for x[i] and
-      0 <= y[i] < C
-
-    Returns a tuple of:
-    - loss: Scalar giving the loss
-    - dx: Gradient of the loss with respect to x
-    """
-    loss, dx = None, None
-
-    ###########################################################################
-    # TODO: Copy over your solution from A1.
-    ###########################################################################
-    # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-    n = x.shape[0]
-    x -= np.max(x, axis=1, keepdims=True)
+    x = x - np.max(x, axis=1, keepdims=True)
     sum_exp_x = np.sum(np.exp(x), axis=1, keepdims=True)
     loss = np.sum(-x[np.arange(n), y] + np.log(sum_exp_x).reshape(-1))
     loss /= n